1ca56eb2ed9dfa06fe995ca874253d3236002565
Otto Kekäläinen <otto@seravo.fi>
2017-07-21 13:47:22 +0300

    Extend settings to include active shipping options and the fixed fee

--- includes/class-wc-pakettikauppa-shipment-method.php
+++ includes/class-wc-pakettikauppa-shipment-method.php
@@ -21,54 +21,96 @@ function wc_pakettikauppa_shipping_method_init() {
     class WC_Pakettikauppa_Shipping_Method extends WC_Shipping_Method {
 
       /**
+       * Default active shipping options.
+       *
+       * @var array
+       */
+      public $active_shipping_options = array(
+        '2103',
+        '2461',
+        '80010',
+        '90010',
+        '90080'
+      );
+
+      /**
+       * Default shipping fee.
+       *
+       * @var int
+       */
+      public $fee = 5;
+
+      /**
       * Constructor for Pakettikauppa shipping class
       *
       * @access public
       * @return void
       */
       public function __construct() {
-
         $this->id = 'WC_Pakettikauppa_Shipping_Method'; // ID for your shipping method. Should be unique.
         $this->method_title = 'Pakettikauppa.fi'; // Title shown in admin
         $this->method_description = __( 'All shipping methods with one contract. For more information visit <a href="https://pakettikauppa.fi/">Pakettikauppa.fi</a>.', 'wc-pakettikauppa' ); // Description shown in admin
+
         $this->enabled = 'yes';
         $this->title = 'Pakettikauppa.fi';
-        $this->init();
 
+        $this->init();
       }
 
       /**
-      * Init settings
-      *
-      * @access public
-      * @return void
-      */
-      function init() {
-        global $wc_pakettikauppa;
-
-        // Load the settings API
+       * Initialize Pakettikauppa.fi shipping
+       */
+      public function init() {
+        // Load the settings.
         $this->init_form_fields();
         $this->init_settings();
 
+        $this->active_shipping_options = $this->get_option( 'active_shipping_options', $this->active_shipping_options );
+        $this->fee = $this->get_option( 'fee', $this->fee );
+
         // Save settings in admin if you have any defined
         add_action( 'woocommerce_update_options_shipping_' . $this->id, array( $this, 'process_admin_options' ) );
-
       }
 
       /**
-       * calculate_shipping function.
-       * @param array $package (default: array())
+       * Init form fields.
+       */
+      public function init_form_fields() {
+        $this->form_fields = array(
+          'active_shipping_options' => array(
+            'title'   => __( 'Active shipping options', 'wc-pakettikauppa' ),
+            'type'    => 'multiselect',
+            'options' => WC_Pakettikauppa::services(),
+          ),
+          'fee' => array(
+            'title'       => __( 'Fixed fee (€)', 'wc-pakettikauppa' ),
+            'type'        => 'price',
+            'description' => __( 'Default fixed price for all Pakettikauppa.fi shipping methods.', 'wc-pakettikauppa' ),
+            'desc_tip'    => true,
+          ),
+        );
+      }
+
+      /**
+       * Called to calculate shipping rates for this method. Rates can be added using the add_rate() method.
+       * Return only active shipping methods.
+       *
+       * @uses WC_Shipping_Method::add_rate()
+       *
+       * @param array $package Shipping package.
        */
       public function calculate_shipping( $package = array() ) {
 
         foreach ( WC_Pakettikauppa::services() as $key => $value ) {
-          $this->add_rate(
-            array(
-              'id'    => $this->id .':'. $key,
-              'label' => $value,
-              'cost'  => 5
-            )
-          );
+          if ( in_array($key, $this->active_shipping_options) ) {
+            $this->add_rate(
+              array(
+                'id'    => $this->id .':'. $key,
+                'label' => $value,
+                'cost'  => $this->fee
+              )
+            );
+          }
 
         }
 
