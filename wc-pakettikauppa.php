1131ddf950e49a707d6ecf77e4a187b8d50c1392
Otto Kekäläinen <otto@seravo.fi>
2017-10-31 23:30:50 +0200

    Save generated PDF files to the uploads folder for universal compatibility

--- wc-pakettikauppa.php
+++ wc-pakettikauppa.php
@@ -26,7 +26,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 // @TODO: Also check for other solutions to refer to plugin_basename and plugin_dir_path in includes/ directory
 define( 'WC_PAKETTIKAUPPA_BASENAME', plugin_basename( __FILE__ ) );
 define( 'WC_PAKETTIKAUPPA_DIR', plugin_dir_path( __FILE__ ) );
-define( 'WC_PAKETTIKAUPPA_PRIVATE_DIR' , $_SERVER['DOCUMENT_ROOT'] . '/../wc-pakettikauppa' );
+$upload_dir = wp_upload_dir();
+define( 'WC_PAKETTIKAUPPA_PRIVATE_DIR' , $upload_dir['basedir'] . '/wc-pakettikauppa' );
+// @TODO: Now the location is unprotected. In future, allow users to customize this
+// location and use techniques like X-Sendfile to limit access to logged in users.
 
 /**
  * Prepare private directory for pakettikauppa documents.
