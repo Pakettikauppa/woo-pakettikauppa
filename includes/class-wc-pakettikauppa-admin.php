bb38ba4ebef8110cd302b61da27b14d7cec1b985
Joose Vettenranta <joose.vettenranta@kompassi.fi>
2017-12-15 09:56:57 +0200

    Default to Finland if no country is present. In the future, put default country to settings.

--- includes/class-wc-pakettikauppa-admin.php
+++ includes/class-wc-pakettikauppa-admin.php
@@ -280,19 +280,12 @@ class WC_Pakettikauppa_Admin {
                 $shipping_address_1 = $order->get_shipping_address_1();
                 $shipping_country = $order->get_shipping_country();
 
-                if ( empty( $shipping_country) ) {
+                if ( empty( $shipping_country )) {
                   $shipping_country = 'FI';
                 }
 
-                foreach ( $active_shipping_options as $shipping_option_id ) {
-                  $shipping_providers[$this->wc_pakettikauppa_shipment->service_provider($shipping_option_id)] = true;
-                }
-
-                $pickup_points = array();
-                foreach($shipping_providers as $_shippingProvider => $_notInUse) {
-                  $pickup_point_data = $this->wc_pakettikauppa_shipment->get_pickup_points( $shipping_postcode, $shipping_address_1, $shipping_country, $_shippingProvider);
-                  $pickup_points = array_merge($pickup_points, json_decode( $pickup_point_data ) );
-                }
+                $pickup_point_data = $this->wc_pakettikauppa_shipment->get_pickup_points( $shipping_postcode, $shipping_address_1, $shipping_country );
+                $pickup_points = json_decode( $pickup_point_data ); ?>
 ?>
                  <div class="form-field" id="wc-pakettikauppa-pickup-points-wrapper">
                    <h4><?php _e( 'Pickup Point', 'wc-pakettikauppa' ); ?></h4>
