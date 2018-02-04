aaeb63716ef490ef120079f1a280239076819cd1
Leo Toikka <leo@seravo.fi>
2018-02-04 14:40:11 +0100

    Fix PHPCS errors by PHPCBF in test files and use Snake_Case for class names

--- tests/test-wc-pk-admin.php
+++ tests/test-wc-pk-admin.php
@@ -1,22 +1,20 @@
 <?php
 
-require_once( WC_PAKETTIKAUPPA_DIR . 'includes/class-wc-pakettikauppa-admin.php' );
+require_once WC_PAKETTIKAUPPA_DIR . 'includes/class-wc-pakettikauppa-admin.php';
 
 /**
- * Class TestWCPakettikauppaAdmin
+ * Class Test_WC_Pakettikauppa_Admin
  *
- * @package Woocommerce_Pakettikauppa
- * @version  1.0.0
-  * @since 1.0.0
-  * @package  woocommerce-pakettikauppa
-  * @author Seravo
+ * @package woocommerce-pakettikauppa
+ * @since 1.0.0
+ * @author Seravo
  */
-class TestWCPakettikauppaAdmin extends WP_UnitTestCase {
+class Test_WC_Pakettikauppa_Admin extends WP_UnitTestCase {
 
   /**
   * Test that the id is set correctly and return an WC_Pakettikauppa_Admin object
   */
-  function test_admin_init() {
+  public function test_admin_init() {
 		$pakettikauppa_admin = new WC_Pakettikauppa_Admin();
 		$this->assertEquals( 'wc_pakettikauppa_admin', $pakettikauppa_admin->id );
 	  $pakettikauppa_admin->load();
@@ -27,7 +25,7 @@ class TestWCPakettikauppaAdmin extends WP_UnitTestCase {
 	/**
 	 * @depends test_admin_init
 	 */
-  function test_admin_errors_empty( $pakettikauppa_admin ) {
+  public function test_admin_errors_empty( $pakettikauppa_admin ) {
 		$this->assertEmpty( $pakettikauppa_admin->get_errors() );
 		return $pakettikauppa_admin;
 	}
@@ -35,7 +33,7 @@ class TestWCPakettikauppaAdmin extends WP_UnitTestCase {
 	/**
 	 * @depends test_admin_errors_empty
 	 */
-	function test_admin_add_error( $pakettikauppa_admin ) {
+	public function test_admin_add_error( $pakettikauppa_admin ) {
     $error = 'This is an admin testing error.';
 		$pakettikauppa_admin->add_error( $error );
 		$this->assertEquals( array( $error ), $pakettikauppa_admin->get_errors() );
@@ -45,7 +43,7 @@ class TestWCPakettikauppaAdmin extends WP_UnitTestCase {
 	/**
 	 * @depends test_admin_add_error
 	 */
-	function test_admin_clear_errors( $pakettikauppa_admin ) {
+	public function test_admin_clear_errors( $pakettikauppa_admin ) {
 		$pakettikauppa_admin->clear_errors();
 		$this->assertEquals( array(), $pakettikauppa_admin->get_errors() );
 	}
