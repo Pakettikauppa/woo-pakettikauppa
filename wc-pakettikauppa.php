dc48aa2227e645ae97f42a09f2a8c8b65688762f
Joose Vettenranta <joose.vettenranta@kompassi.fi>
2018-10-25 10:54:18 +0300

    reformatting code again to match requirements

--- wc-pakettikauppa.php
+++ wc-pakettikauppa.php
@@ -19,47 +19,47 @@
  */
 
 // Prevent direct access to this script
-if(!defined('ABSPATH')) {
+if ( ! defined ('ABSPATH')) {
     exit;
 }
 
 // @TODO: Also check for other solutions to refer to plugin_basename and plugin_dir_path in includes/ directory
-define('WC_PAKETTIKAUPPA_BASENAME', plugin_basename(__FILE__));
-define('WC_PAKETTIKAUPPA_DIR', plugin_dir_path(__FILE__));
+define ('WC_PAKETTIKAUPPA_BASENAME', plugin_basename (__FILE__));
+define ('WC_PAKETTIKAUPPA_DIR', plugin_dir_path (__FILE__));
 
 /**
  * Load plugin textdomain
  *
  * @return void
  */
-function wc_pakettikauppa_load_textdomain() {
-    load_plugin_textdomain('wc-pakettikauppa', false, dirname(plugin_basename(__FILE__)) . '/languages/');
+function wc_pakettikauppa_load_textdomain () {
+    load_plugin_textdomain ('wc-pakettikauppa', false, dirname (plugin_basename (__FILE__)) . '/languages/');
 }
 
-add_action('plugins_loaded', 'wc_pakettikauppa_load_textdomain');
+add_action ('plugins_loaded', 'wc_pakettikauppa_load_textdomain');
 
-require_once plugin_dir_path(__FILE__) . 'includes/class-wc-pakettikauppa-shipping-method.php';
+require_once plugin_dir_path (__FILE__) . 'includes/class-wc-pakettikauppa-shipping-method.php';
 
 /**
  * Load the WC_Pakettikauppa class when in frontend
  */
-function wc_pakettikauppa_load() {
-    if(!is_admin()) {
-        require_once plugin_dir_path(__FILE__) . 'includes/class-wc-pakettikauppa.php';
+function wc_pakettikauppa_load () {
+    if ( ! is_admin ()) {
+        require_once plugin_dir_path (__FILE__) . 'includes/class-wc-pakettikauppa.php';
         $wc_pakettikauppa = new WC_Pakettikauppa();
-        $wc_pakettikauppa->load();
+        $wc_pakettikauppa->load ();
     }
 }
 
-add_action('init', 'wc_pakettikauppa_load');
+add_action ('init', 'wc_pakettikauppa_load');
 
 /**
  * Load the WC_Pakettikauppa_Admin class in wp-admin
  */
-function wc_pakettikauppa_load_admin() {
-    require_once plugin_dir_path(__FILE__) . 'includes/class-wc-pakettikauppa-admin.php';
+function wc_pakettikauppa_load_admin () {
+    require_once plugin_dir_path (__FILE__) . 'includes/class-wc-pakettikauppa-admin.php';
     $wc_pakettikauppa_admin = new WC_Pakettikauppa_Admin();
-    $wc_pakettikauppa_admin->load();
+    $wc_pakettikauppa_admin->load ();
 }
 
-add_action('admin_init', 'wc_pakettikauppa_load_admin');
+add_action ('admin_init', 'wc_pakettikauppa_load_admin');
