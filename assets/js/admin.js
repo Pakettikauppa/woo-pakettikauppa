8e14314ddf0ba7491d2dd83a191760625f93c01a
Aidas Subačius <aidas.subacius@gmail.com>
2020-09-15 17:13:41 +0300

    Fixed issue #143

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,57 +1,66 @@
 // phpcs:disable PEAR.Functions.FunctionCallSignature
 /* admin js */
 jQuery(function( $ ) {
-  window.pakettikauppa_meta_box_submit = function(obj) {
-    $('#woo-pakettikauppa').block({
-      message: null,
-      overlayCSS: {
-        background: '#fff',
-        opacity: 0.6
-      }
-    });
+  var submitCount = 0;
+      window.pakettikauppa_meta_box_submit = function(obj) {
 
-    var data = {
-      action: 'pakettikauppa_meta_box',
-      post_id: woocommerce_admin_meta_boxes.post_id,
-      security: $('#pakettikauppa_metabox_nonce').val()
-    };
+        console.log(submitCount);
+        if (submitCount === 0){
+          submitCount++;
+          $('#woo-pakettikauppa').block({
+            message: null,
+            overlayCSS: {
+              background: '#fff',
+              opacity: 0.6
+            }
+          });
 
-    if ($("#wc_pakettikauppa_shipping_method").is(':visible')) {
-      $("#wc_pakettikauppa_custom_shipping_method").html('');
+        var data = {
+          action: 'pakettikauppa_meta_box',
+          post_id: woocommerce_admin_meta_boxes.post_id,
+          security: $('#pakettikauppa_metabox_nonce').val()
+        };
 
-      $('.pakettikauppa_metabox_values').each(function (i, obj) {
-        var name = $(obj).attr('name');
-        data[name] = $(this).val();
-      });
-    } else {
-      $("#wc_pakettikauppa_shipping_method").html('');
+        if ($("#wc_pakettikauppa_shipping_method").is(':visible')) {
+          $("#wc_pakettikauppa_custom_shipping_method").html('');
 
-      var shipping_method = $('#pakettikauppa-service').val();
+          $('.pakettikauppa_metabox_values').each(function (i, obj) {
+            var name = $(obj).attr('name');
+            data[name] = $(this).val();
+          });
+        } else {
+          $("#wc_pakettikauppa_shipping_method").html('');
 
-      data['wc_pakettikauppa_service_id'] = shipping_method;
-      data['additional_services'] = [];
-      data['custom_method'] = 1;
+          var shipping_method = $('#pakettikauppa-service').val();
 
-      $('#pk-admin-additional-services-' + shipping_method + ' .pakettikauppa_metabox_values').each(function (i, obj) {
-        var name = $(obj).attr('name');
-        data[name] = $(this).val();
-      });
+          data['wc_pakettikauppa_service_id'] = shipping_method;
+          data['additional_services'] = [];
+          data['custom_method'] = 1;
 
-      $('#pk-admin-additional-services-' + shipping_method + ' .pakettikauppa_metabox_array_values').each(function (i, obj) {
-        if ($(this).prop("checked")) {
-          data['additional_services'].push($(this).val());
+          $('#pk-admin-additional-services-' + shipping_method + ' .pakettikauppa_metabox_values').each(function (i, obj) {
+            var name = $(obj).attr('name');
+            data[name] = $(this).val();
+          });
+
+          $('#pk-admin-additional-services-' + shipping_method + ' .pakettikauppa_metabox_array_values').each(function (i, obj) {
+            if ($(this).prop("checked")) {
+              data['additional_services'].push($(this).val());
+            }
+          });
         }
-      });
-    }
 
-    data[$(obj).attr('name')] = $(obj).val();
+        data[$(obj).attr('name')] = $(obj).val();
 
-    $.post(woocommerce_admin_meta_boxes.ajax_url, data, function(response) {
-      $("#woo-pakettikauppa .inside").html(response);
-      $('#woo-pakettikauppa').unblock();
-    }).fail(function() {
-      location.reload();
-    });
+        $.post(woocommerce_admin_meta_boxes.ajax_url, data, function (response) {
+          $("#woo-pakettikauppa .inside").html(response);
+          $('#woo-pakettikauppa').unblock();
+          console.log(response);
+          submitCount = 0;
+        }).fail(function () {
+          location.reload();
+        });
+
+      }
   };
 
   window.pakettikauppa_change_method = function(obj) {
