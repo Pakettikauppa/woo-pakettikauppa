34ea31eda9878b9dd7e9197e780830ad2e8d835c
Kestutis M <pampyras@gmail.com>
2021-12-16 17:49:06 +0200

    Show preselected additional services in original shipment

--- core/class-admin.php
+++ core/class-admin.php
@@ -832,7 +832,7 @@ if ( ! class_exists(__NAMESPACE__ . '\Admin') ) {
       $order_postcode = $order->get_shipping_postcode();
       $order_address  = $order->get_shipping_address_1() . ' ' . $order->get_shipping_city();
       $order_country  = $order->get_shipping_country();
-      $is_cod = $order->get_payment_method() == 'cod';
+      $is_cod = $order->get_payment_method() === 'cod';
       ?>
       <div>
         <input type="hidden" name="pakettikauppa_nonce" value="<?php echo wp_create_nonce(str_replace('wc_', '', $this->core->prefix) . '-meta-box'); ?>" id="pakettikauppa_metabox_nonce" />
@@ -996,6 +996,10 @@ if ( ! class_exists(__NAMESPACE__ . '\Admin') ) {
                 <?php endif; ?>
               <?php endforeach; ?>
             </fieldset>
+              <fieldset id = "default_shipment_additional_services">
+                  <ol style="list-style: circle;">
+                  </ol>
+              </fieldset>
           </div>
           <?php $this->tpl_products_selector($order); ?>
           <p class="pakettikauppa-metabox-footer">
