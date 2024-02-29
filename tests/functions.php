2c84624c561d9e727b6d09fcf65eb7d6d0832d53
Marijus <markakk@gmail.com>
2024-02-29 17:12:07 +0200

    Find plugin main file on renamed directory

--- tests/functions.php
+++ tests/functions.php
@@ -1,5 +1,24 @@
 <?php
 
+function is_file_contains_text($path_to_file, $text) {
+  if ( empty($path_to_file) ) {
+    return false;
+  }
+
+  $found = false;
+
+  $file = fopen($path_to_file, 'r');
+  while (($buffer = fgets($file)) !== false) {
+    if (strpos($buffer, $text) !== false) {
+      $found = true;
+      break;
+    }
+  }
+  fclose($file);
+
+  return $found;
+}
+
 function get_plugin_class() {
   $dir = basename(get_plugin_directory(true));
   $class = '';
@@ -25,7 +44,16 @@ function get_plugin_directory( $replace = false ) {
 }
 
 function get_plugin_main_filename() {
-  return basename(get_plugin_directory(true)) . '.php';
+  $main_file = basename(get_plugin_directory(true)) . '.php';
+  $files = glob(get_plugin_directory(true) . '/*.php');
+  foreach ( $files as $file ) {
+    if ( is_file_contains_text($file, 'Plugin Name:') ) {
+      $main_file = basename($file);
+      break;
+    }
+  }
+  
+  return $main_file;
 }
 
 function get_plugin_config() {
